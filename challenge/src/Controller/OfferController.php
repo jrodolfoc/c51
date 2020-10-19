cfaed0011fe88f1dd39939da60be0db5af13322c
jrodolfoc <jrodolfoc@gmail.com>
2020-10-19 00:56:32 -0400

    Add sorting to name and cash back columns

--- challenge/src/Controller/OfferController.php
+++ challenge/src/Controller/OfferController.php
@@ -19,17 +19,45 @@ class OfferController extends AbstractController
 {
     /**
      * Display all offers
-     * @Route("/", methods="GET", name="offer_index")
+     * @Route("/{sort}/{direction}", defaults={"sort": "name", "direction"="asc"}, methods="GET", name="offer_index")
      * @param OfferRepository $offerRepository
+     * @param string $sort
+     * @param string $direction
      * @return Response
      */
-    public function index(OfferRepository $offerRepository): Response
+    public function index(OfferRepository $offerRepository, string $sort, string $direction): Response
     {
+        if (!in_array(strtolower($sort), ['name', 'cash_back'])) {
+            $sort = 'name';
+        }
+
+        if (!in_array(strtolower($direction), ['asc', 'desc'])) {
+            $direction = 'asc';
+        }
+
         /** @var Offer[] $offers */
-        $offers = $offerRepository->findAll();
+        $offers = $offerRepository->findBy([], [$sort => $direction]);
 
         return $this->render('offer/index.html.twig', [
             'offers' => $offers,
+            'sort' => $sort,
+            'nameDirection' => $this->calculateNextDirection('name', $sort, $direction),
+            'cashDirection' => $this->calculateNextDirection('cash_back', $sort, $direction),
         ]);
+    }
+
+    /**
+     * @param $column
+     * @param $currentSort
+     * @param $currentDirection
+     * @return string
+     */
+    private function calculateNextDirection($column, $currentSort, $currentDirection)
+    {
+        if ($column != $currentSort) {
+            return 'asc';
+        }
+
+        return $currentDirection == 'asc' ? 'desc' : 'asc';
     }
 }
